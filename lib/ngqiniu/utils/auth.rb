4a69c51c4bc05f3c9ff71c32b274076d86cd436a
Bachue Zhou <bachue.shu@gmail.com>
2019-06-21 11:46:47 +0800

    finish auth module

--- lib/ngqiniu/utils/auth.rb
+++ lib/ngqiniu/utils/auth.rb
@@ -23,18 +23,47 @@ module Ngqiniu
         "#{sign(encoded_data)}:#{encoded_data}"
       end
 
-      def sign_request(url, content_type:, body: nil)
-        form_mime = 'application/x-www-form-urlencoded'
+      def sign_request(url, version: 1, method: 'GET', content_type:, body: nil)
+        case version
+        when 1 then sign_request_v1(url, content_type: content_type, body: body)
+        when 2 then sign_request_v2(url, method: method, content_type: content_type, body: body)
+        else raise NotImplementedError
+        end
+      end
+
+      def authorization_for_request(url, version: 1, method: 'GET', content_type:, body: nil)
+        case version
+        when 1 then 'QBox ' + sign_request_v1(url, content_type: content_type, body: body)
+        when 2 then 'Qiniu ' + sign_request_v2(url, method: method, content_type: content_type, body: body)
+        else raise NotImplementedError
+        end
+      end
+
+      def sign_request_v1(url, content_type:, body: nil)
         uri = URI(url)
         data_to_sign = uri.path.encode('UTF-8')
         data_to_sign += '?' + uri.query.encode('UTF-8') unless uri.query.nil?
         data_to_sign += "\n"
-        data_to_sign += body if !body.nil? && form_mime.casecmp?(content_type)
+        data_to_sign += body if inc_body_v1?(content_type, body)
         sign(data_to_sign)
       end
 
-      def callback_valid?(original_authorization, url, content_type:, body: nil)
-        original_authorization == "QBox #{sign_request(url, body: body, content_type: content_type)}"
+      def sign_request_v2(url, method: 'GET', content_type:, body: nil)
+        uri = URI(url)
+        data_to_sign = "#{method.upcase} #{uri.path.encode('UTF-8')}"
+        data_to_sign += '?' + uri.query.encode('UTF-8') unless uri.query.nil?
+        data_to_sign += "\nHost: #{uri.host}"
+        data_to_sign += ":#{uri.port}" if uri.port != uri.default_port
+        data_to_sign += "\n"
+        data_to_sign += "Content-Type: #{content_type}\n" unless content_type.nil? || content_type.empty?
+        data_to_sign += "\n"
+        data_to_sign += body if inc_body_v2?(content_type, body)
+        sign(data_to_sign)
+      end
+
+      def callback_valid?(original, url, method: 'POST', content_type:, body: nil)
+        expected = authorization_for_request(url, version: 1, method: method, body: body, content_type: content_type)
+        original == expected
       end
 
       def sign_download_url_with_deadline(base_url, deadline:)
@@ -51,6 +80,7 @@ module Ngqiniu
       end
 
       def sign_download_url_with_lifetime(base_url, lifetime:)
+        lifetime = Duration.new(lifetime) if lifetime.is_a?(Hash)
         deadline = [Time.now.to_i + lifetime.to_i, (1 << 32) - 1].min
         sign_download_url_with_deadline(base_url, deadline: deadline)
       end
@@ -59,6 +89,17 @@ module Ngqiniu
 
       def hmac_digest(data)
         OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'), @secret_key, data)
+      end
+
+      def inc_body_v1?(content_type, body)
+        form_mime = 'application/x-www-form-urlencoded'
+        !body.nil? && form_mime.casecmp?(content_type)
+      end
+
+      def inc_body_v2?(content_type, body)
+        form_mime = 'application/x-www-form-urlencoded'
+        json_mime = 'application/json'
+        !body.nil? && (form_mime.casecmp?(content_type) || json_mime.casecmp?(content_type))
       end
     end
   end
--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -4,31 +4,101 @@ require_relative 'spec_helper'
 
 RSpec.describe Ngqiniu::Utils do
   describe Ngqiniu::Utils::Auth do
-    dummy_access_key = 'abcdefghklmnopq'
-    dummy_secret_key = '1234567890'
-    dummy_auth = Ngqiniu::Utils::Auth.new(access_key: dummy_access_key, secret_key: dummy_secret_key)
+    describe '#sign' do
+      dummy_access_key = 'abcdefghklmnopq'
+      dummy_secret_key = '1234567890'
+      dummy_auth = Ngqiniu::Utils::Auth.new(access_key: dummy_access_key, secret_key: dummy_secret_key)
 
-    it 'should sign correctly' do
-      expect(dummy_auth.sign('test')).to eq 'abcdefghklmnopq:mSNBTR7uS2crJsyFr2Amwv1LaYg='
+      it 'should sign correctly' do
+        expect(dummy_auth.sign('hello')).to eq 'abcdefghklmnopq:b84KVc-LroDiz0ebUANfdzSRxa0='
+        expect(dummy_auth.sign('world')).to eq 'abcdefghklmnopq:VjgXt0P_nCxHuaTfiFz-UjDJ1AQ='
+        expect(dummy_auth.sign('-test')).to eq 'abcdefghklmnopq:vYKRLUoXRlNHfpMEQeewG0zylaw='
+        expect(dummy_auth.sign('ba#a-')).to eq 'abcdefghklmnopq:2d_Yr6H1GdTKg3RvMtpHOhi047M='
+      end
+
+      it 'should sign with data correctly' do
+        expect(dummy_auth.sign_with_data('hello')).to eq 'abcdefghklmnopq:BZYt5uVRy1RVt5ZTXbaIt2ROVMA=:aGVsbG8='
+        expect(dummy_auth.sign_with_data('world')).to eq 'abcdefghklmnopq:Wpe04qzPphiSZb1u6I0nFn6KpZg=:d29ybGQ='
+        expect(dummy_auth.sign_with_data('-test')).to eq 'abcdefghklmnopq:HlxenSSP_6BbaYNzx1fyeyw8v1Y=:LXRlc3Q='
+        expect(dummy_auth.sign_with_data('ba#a-')).to eq 'abcdefghklmnopq:kwzeJrFziPDMO4jv3DKVLDyqud0=:YmEjYS0='
+      end
+
+      it 'should sign request correctly' do
+        expect(dummy_auth.sign_request('', content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('', content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('', method: 'GET', content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('', method: 'POST', content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("\n")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("\nname=test&language=go")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com?v=2', content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("?v=2\nname=test&language=go")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com/find/sdk?v=2', content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("/find/sdk?v=2\nname=test&language=go")
+        )
+      end
+
+      it 'should sign request v2 correctly' do
+        expect(dummy_auth.sign_request('', version: 2, content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("GET \nHost: \nContent-Type: application/json\n\n{\"name\": \"test\"}")
+        )
+        expect(dummy_auth.sign_request('', version: 2, content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("GET \nHost: \n\n")
+        )
+        expect(dummy_auth.sign_request('', method: 'GET', version: 2, content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("GET \nHost: \n\n")
+        )
+        expect(dummy_auth.sign_request('', method: 'POST', version: 2, content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("POST \nHost: \nContent-Type: application/json\n\n{\"name\": \"test\"}")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', version: 2, content_type: nil, body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("GET \nHost: upload.qiniup.com\n\n")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', version: 2, content_type: 'application/json', body: '{"name": "test"}')).to(
+          eq dummy_auth.sign("GET \nHost: upload.qiniup.com\nContent-Type: application/json\n\n{\"name\": \"test\"}")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com', version: 2, content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("GET \nHost: upload.qiniup.com\nContent-Type: application/x-www-form-urlencoded\n\nname=test&language=go")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com?v=2', version: 2, content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("GET ?v=2\nHost: upload.qiniup.com\nContent-Type: application/x-www-form-urlencoded\n\nname=test&language=go")
+        )
+        expect(dummy_auth.sign_request('http://upload.qiniup.com/find/sdk?v=2', version: 2, content_type: 'application/x-www-form-urlencoded', body: 'name=test&language=go')).to(
+          eq dummy_auth.sign("GET /find/sdk?v=2\nHost: upload.qiniup.com\nContent-Type: application/x-www-form-urlencoded\n\nname=test&language=go")
+        )
+      end
     end
 
-    it 'should sign with data correctly' do
-      expect(dummy_auth.sign_with_data('test')).to eq 'abcdefghklmnopq:-jP8eEV9v48MkYiBGs81aDxl60E=:dGVzdA=='
-    end
+    describe '#sign_download_url_with_deadline' do
+      it 'should sign download url correctly' do
+        dummy_auth = Ngqiniu::Utils::Auth.new(access_key: 'abcdefghklmnopq', secret_key: '1234567890')
+        expect(dummy_auth.sign_download_url_with_deadline('http://www.qiniu.com?go=1', deadline: Time.at(1_234_567_890 + 3600))).to(
+          eq 'http://www.qiniu.com?go=1&e=1234571490&token=abcdefghklmnopq:8vzBeLZ9W3E4kbBLFLW0Xe0u7v4='
+        )
+      end
 
-    it 'should sign request correctly' do
-      expect(dummy_auth.sign_request('http://www.qiniu.com?go=1', body: 'test', content_type: '')).to(
-        eq 'abcdefghklmnopq:cFyRVoWrE3IugPIMP5YJFTO-O-Y='
-      )
-      expect(dummy_auth.sign_request('http://www.qiniu.com?go=1', body: 'test', content_type: 'application/x-www-form-urlencoded')).to(
-        eq 'abcdefghklmnopq:svWRNcacOE-YMsc70nuIYdaa1e4='
-      )
-    end
-
-    it 'should sign download url correctly' do
-      expect(dummy_auth.sign_download_url_with_deadline(
-               'http://www.qiniu.com?go=1', deadline: Time.at(1_234_567_890 + 3600)
-             )).to eq 'http://www.qiniu.com?go=1&e=1234571490&token=abcdefghklmnopq:8vzBeLZ9W3E4kbBLFLW0Xe0u7v4='
+      %w[4k 1m 4m 8m 16m 64m 1g].each do |size|
+        it "should download #{size} file correctly" do
+          real_auth = Ngqiniu::Utils::Auth.new(access_key: access_key, secret_key: secret_key)
+          url = real_auth.sign_download_url_with_lifetime("http://z1-bucket.kodo-test.qiniu-solutions.com/#{size}", lifetime: { seconds: 30 })
+          expect(Faraday.head(url)).to be_success
+        end
+      end
     end
   end
 end
